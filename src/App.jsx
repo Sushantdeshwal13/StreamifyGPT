dc781452ea294656ee6dff5635eb583c64360548
sushant <sushantdeshwal13@gmail.com>
2025-05-28 12:40:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className='text-4xl font-bold text-green-800'>
+      Namaste, everyone, lets build</div>
+    </>
+  )
+}
+
+export default App
